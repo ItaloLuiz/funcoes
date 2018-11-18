ebde7c100ea1b3fd56d5e4fbacdb85a82f151824
ItaloLuiz <45146932+ItaloLuiz@users.noreply.github.com>
2018-11-18 13:43:51 -0200

    Create gerasenha.php

---
+++ gerasenha.php
@@ -0,0 +1,15 @@
+function GeraSenha($qtda,$esp = false){
+	if($qtda <=3){
+		$qtda = 4;
+	}
+	if($esp == true){
+		$carac = '@!#$%*-^~';
+	}else{
+		$carac = '';
+	}
+	$str = 'abcdefghijklmnopqrstuvxzyw0123456789ABDEFGHIJKLMNOPQRSTUVXZYW'.$carac;
+	$shuffle = str_shuffle($str);
+	return substr($shuffle,mt_rand(0,2),$qtda);
+}
+
+echo GeraSenha(6,false);
