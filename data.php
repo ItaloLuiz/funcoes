73ff3d9fce9c51b7499c778e79266a5c6950f29c
ItaloLuiz <45146932+ItaloLuiz@users.noreply.github.com>
2020-01-13 13:44:15 -0300

    caso seja sábado ou domingo setar data pra segunda

---
+++ data.php
@@ -0,0 +1,13 @@
+function ProximaSegunda($data)
+{
+	$dia = date('w', strtotime($data));
+	if ($dia == '0') {
+		return date('Y-m-d', strtotime("+1 days", strtotime($data)));
+	} else if ($dia == '6') {
+		return date('Y-m-d', strtotime("+2 days", strtotime($data)));
+	} else {
+		return $data;
+	}
+}
+
+echo ProximaSegunda($data);
